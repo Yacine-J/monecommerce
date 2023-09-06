b7ca200fc05bfff504fb956a627213c8b838db5b
Yacine-J <yacinej@outlook.fr>
2023-09-06 11:10:52 +0200

    initial commit

---
+++ inc/functions.inc.php
@@ -0,0 +1,27 @@
+<!-- toutes nos fonctions -->
+<!-- Execution requête -->
+<?php
+function executeRequete($req) {
+    global $mysqli;
+    try {
+        $resultat = $mysqli->query($req);
+        if (!$resultat) {
+            die("Erreur sur la requête sql.<br>Message : " . $mysqli->error . "<br>Code : " . $req);
+        }
+    } catch (Exception $e) {
+        return $e;
+    }
+}
+// Affichage de notre debogage
+function debug($var, $mode = 1) {
+    echo '<div style="background: lightgrey; padding: 5px; float: right; clear: both; ">';
+    $trace = debug_backtrace();
+    $trace = array_shift($trace);
+    echo "Debug demandé dans le fichier : $trace[file] à la ligne $trace[line].";
+    if($mode === 1) {
+        echo '<pre>'; print_r($var); echo '</pre>';
+    } else {
+        echo '<pre>'; var_dump($var); echo '</pre>';
+    }
+    echo '</div>';
+}
--- index.php
+++ index.php
@@ -6,4 +6,13 @@ require('./inc/haut.inc.php');
 <h2>Notre page d'accueil</h2>
 <p>Coincée entre le haut et le bas!</p>
 <?php
+
+
+// echo getenv('NOM_VARIABLE');
+// echo '<br>';
+// echo $_ENV['NOM_VARIABLE'];
+
+// echo $_ENV['TEST'];
+
+
 require('./inc/bas.inc.php');
